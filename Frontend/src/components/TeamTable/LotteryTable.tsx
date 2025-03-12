5c2295451021bd15d44f27f412aa923098c86ccb
MiracleGuardian91 <miracleguardian91@gmail.com>
2025-03-12 16:02:55 +0900

    Updated lottery table display mode

--- Frontend/src/components/TeamTable/LotteryTable.tsx
+++ Frontend/src/components/TeamTable/LotteryTable.tsx
@@ -1,20 +1,56 @@
+import { useEffect, useState } from "react";
 import { Team, useTeamStore } from "../../store/team.store";
 
 const LotteryTable = () => {
-  const { teams, lotteryTeams } = useTeamStore() as {
+  const { teams, isLotteryStarted, lotteryTeams } = useTeamStore() as {
     teams: Team[];
+    isLotteryStarted: boolean;
     lotteryTeams: Team[] | null;
   };
-
-  const sortedTeams = teams.sort((a, b) =>
-    a.team_name.localeCompare(b.team_name)
+  const blankRows: Team[] = new Array(teams.length).fill({
+    team_name: "",
+  } as Team);
+  const sortedTeams = [...teams].sort(
+    (a, b) => a.team_name.localeCompare(b.team_name)
   );
+  const [_, setCurrentIndex] = useState<number>(-1);
+  const [displayTeams, setDisplayedTeams] = useState<Team[]>(blankRows);
 
-  const blankRows: Team[] = new Array(teams.length).fill({ team_name: '' } as Team);
-  const displayTeams = lotteryTeams ? [
-    ...lotteryTeams,
-    ...blankRows.slice(0, sortedTeams.length - lotteryTeams.length),
-  ] : blankRows;
+  useEffect(() => {
+    let interval: NodeJS.Timeout | undefined;
+
+    if (isLotteryStarted) {
+      interval = setInterval(() => {
+        setCurrentIndex((prevIndex) => {
+          const nextIndex = prevIndex + 1;
+          if (nextIndex >= teams.length) {
+            clearInterval(interval);
+            return prevIndex;
+          }
+
+          let updatedDisplayTeams = [...displayTeams];
+          const lotteryIndex =
+            lotteryTeams?.findIndex(
+              (team) => team.team_name === sortedTeams[nextIndex].team_name
+            ) ?? -1;
+
+          if (lotteryIndex !== -1 && lotteryTeams) {
+            updatedDisplayTeams[lotteryIndex] = lotteryTeams[lotteryIndex];
+            setDisplayedTeams([...updatedDisplayTeams]);
+          }
+
+          return nextIndex;
+        });
+      }, 1000);
+    } else {
+      setCurrentIndex(-1);
+      if (interval) clearInterval(interval);
+    }
+
+    return () => {
+      if (interval) clearInterval(interval);
+    };
+  }, [isLotteryStarted, lotteryTeams, displayTeams]);
 
   return (
     <div className="rounded-sm border border-stroke shadow-default dark:border-strokedark">
@@ -31,40 +67,33 @@ const LotteryTable = () => {
             </tr>
           </thead>
           <tbody>
-            {displayTeams.length > 0 ? (
-              displayTeams.slice().map((team, index) => (
-                <tr
-                  key={index}
-                  className={` bg-white dark:bg-boxdark whitespace-nowrap cursor-pointer dark:text-white ${
-                    index === displayTeams.length - 1
-                      ? ""
-                      : "border-b border-b-stroke dark:border-b-strokedark"
+            {displayTeams.slice().map((team, index) => (
+              <tr
+                key={index}
+                className={` bg-white dark:bg-boxdark whitespace-nowrap cursor-pointer dark:text-white ${
+                  index === displayTeams.length - 1
+                    ? ""
+                    : "border-b border-b-stroke dark:border-b-strokedark"
+                }`}
+              >
+                <td
+                  className={`w-1/12 ${
+                    team.team_name === "" ? "py-5" : "px-6 py-2 "
                   }`}
                 >
-                  <td className={`w-1/12 ${sortedTeams.findIndex(
-                      (item) => item.team_name === team.team_name
-                    ) === -1 ? 'py-5' : 'px-6 py-2 '}`}>
-                    {sortedTeams.findIndex(
-                      (item) => item.team_name === team.team_name
-                    ) !== -1
-                      ? sortedTeams.findIndex(
+                  {team.team_name
+                    ? teams?.findIndex(
+                        (item) => item.team_name === team.team_name
+                      ) !== undefined
+                      ? teams.findIndex(
                           (item) => item.team_name === team.team_name
                         ) + 1
-                      : ""}
-                  </td>
-                  <td className="px-6 py-2 w-11/12">{team.team_name}</td>
-                </tr>
-              ))
-            ) : (
-              <tr className="border-t border-t-stroke dark:border-t-strokedark">
-                <td
-                  colSpan={14}
-                  className="px-10 py-3 text-black dark:text-white text-lg"
-                >
-                  No teams
+                      : ""
+                    : ""}
                 </td>
+                <td className="px-6 py-2 w-11/12">{team.team_name}</td>
               </tr>
-            )}
+            ))}
           </tbody>
         </table>
       </div>
