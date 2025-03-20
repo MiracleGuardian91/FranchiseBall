afd1517d86b0553a19752bceceba3f5e7666aa28
MiracleGuardian91 <miracleguardian91@gmail.com>
2025-03-20 14:52:02 +0900

    Crawler code updated for mongodb and Model updated in backend

---
+++ Backend/src/models/Player.ts
@@ -0,0 +1,65 @@
+import mongoose, {Schema, Document} from "mongoose";
+
+export interface IPlayer extends Document {
+  link: string;
+  name: string;
+  position: string;
+  age: number;
+  control: number;
+  movement: number;
+  velocity: number;
+  stamina: number;
+  power: number;
+  contact: number;
+  speed: number;
+  defense: number
+}
+
+const PlayerSchema = new Schema({
+  link: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  name: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  position: {
+    type: String,
+    required: true
+  },
+  age: {
+    type: Number,
+    required: true
+  },
+  control: {
+    type: Number
+  },
+  movement: {
+    type: Number
+  },
+  velocity: {
+    type: Number
+  },
+  stamina: {
+    type: Number
+  },
+  power: {
+    type: Number
+  },
+  contact: {
+    type: Number
+  },
+  speed: {
+    type: Number
+  },
+  defense: {
+    type: Number
+  }
+})
+
+const Player = mongoose.model<IPlayer>("Player", PlayerSchema);
+
+export default Player;
